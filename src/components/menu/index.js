c58ccb1818be16f7dda2db5e9ae4aaf88c093eaf
MarianaMaiko <marianamaikoht@gmail.com>
2021-02-06 10:19:15 -0300

    Initall commit

---
+++ src/components/menu/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+export default function Menu() {
+    return(
+    <div className='texte'>        
+        <h1>Site Loko de hackatons e pah</h1>
+    </div>
+    )
+};
