74e54364a4ca3266889ca9851d13a3a157b62d73
Tassia Accioly <tassia.accioly@gmail.com>
2021-02-07 02:17:27 -0300

    feat: start front auth and routing

---
+++ src/apis/challengesApi.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const DEV_API = "http://localhost:1234/api";
+const PROD_API = "";
+
+const api = axios.create({
+  baseURL: process.env.NODE_ENV === "production" ? PROD_API : DEV_API,
+});
+
+const storedUser = localStorage.getItem("loggedInUser");
+
+const loggedInUser = JSON.parse(storedUser || '""');
+
+api.interceptors.request.use((config) => {
+  config.headers = {
+    Authorization: `Bearer ${loggedInUser.token}`,
+  };
+
+  return config;
+});
+
+export default api;
